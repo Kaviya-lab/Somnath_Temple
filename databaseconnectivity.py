94e89a3d9e23b1831f2fc1fc38d0a06754ac5d80
mahimaa-kabilan <mahimaa1012@gmail.com>
2025-09-21 21:03:15 +0530

    Add files via upload

--- databaseconnectivity.py
+++ databaseconnectivity.py
@@ -1,5 +1,5 @@
 import sqlite3
-conn = sqlite3.connect("somnath_temple.db") 
+conn = sqlite3.connect("somnath_temple_data.db") 
 cursor = conn.cursor() 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
@@ -16,10 +16,21 @@ CREATE TABLE IF NOT EXISTS bookings (
     user_id INTEGER,
     date TEXT NOT NULL,
     time TEXT NOT NULL,
+    no_of_pl INTEGER,
     qr_code TEXT UNIQUE,
     status TEXT DEFAULT 'Confirmed',
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 )
+""")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS persons (
+    person_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    booking_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    age INTEGER,
+    gender TEXT,
+    FOREIGN KEY (booking_id) REFERENCES bookings(booking_id) ON DELETE CASCADE
+);
 """)
 conn.commit()  # save changes to database
 conn.close()   # close connection
